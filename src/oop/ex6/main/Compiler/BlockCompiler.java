eb1f6482eb211316b7c9fbc2eee80bc215084b23
tomernx <tomer.lankri@mail.huji.ac.il>
2018-06-09 19:52:58 +0300

    done we can file the ex

--- src/oop/ex6/main/Compiler/BlockCompiler.java
+++ src/oop/ex6/main/Compiler/BlockCompiler.java
@@ -8,11 +8,14 @@ public class BlockCompiler extends FileCompiler {
 
     protected FileCompiler myCompiler;
     private BlockCompiler parentBlock = null;
-     int start;
-     int end;
+    int start;
+    int end;
+    private static final String  VAR_ASSIGNMENT = "(([a-zA-Z]*|[_])[\\w]+)[\\s]+[=].*";
 
-     enum lineCase{
-         IF_WHILE,FUNCTION_CALL,VAR_USAGE,RETURN,END_BLOCK
+    private static final String VAR_DECLERATION = "(final)?[\\s]+(int|double|char|boolean|String)[\\s].*";
+
+    enum LineCase {
+         IF_WHILE,FUNCTION_CALL,VAR_USAGE,RETURN,END_BLOCK,NO_MATCH;
      }
 
 
@@ -36,7 +39,21 @@ public class BlockCompiler extends FileCompiler {
         // first off we check if the last 2 lines contains the return and "}"  statement.
         checkReturnStatment();
         for (int i = start; i < end; i++) {
-
+            LineCase lineCase = getLineCase(code.get(i));
+            switch (lineCase){
+                case RETURN:
+                    break;
+                case END_BLOCK:
+                    break;
+                case IF_WHILE:
+                    break;
+                case VAR_USAGE:
+                    break;
+                case FUNCTION_CALL:
+                    break;
+                case NO_MATCH:
+                    throw new Exception("bad line format");
+            }
         }
 
         //compiling my subBlocks ?
@@ -50,37 +67,42 @@ public class BlockCompiler extends FileCompiler {
     }
 
 //](true|false|[-]?[0-9]+[.]?[0-9]*|[-]?[.][0-9]+)[)][\s]*[{][\s]*$
-    private  lineCase getLineCase(String line){
+    private LineCase getLineCase(String line){
 
         // if or while case.
         Pattern p =  Pattern.compile("^[\\s]*(if|while)[\\s]*[(].+[)][\\s]*[{]");
         Matcher m = p.matcher(line);
 
-        if(m.matches()){ return lineCase.IF_WHILE;}
+        if(m.matches()){ return LineCase.IF_WHILE;}
 
         // end of block case
         p =  Pattern.compile("^[\\s]*}[\\s]*$");
         m = p.matcher(line);
         if (m.matches()){
-            return lineCase.END_BLOCK;
+            return LineCase.END_BLOCK;
         }
 
+
+        //^[\s]*(return;)[\s]*^
+        //[\\s]*((final)?[\\s]+(int|double|char|boolean|String)[\\s].*|(([a-zA-Z]*|[_])[\\w]+)[\\s]+[=].*)
         // return line case.
         p =  Pattern.compile("^[\\s]*(return;)[\\s]*^");
         m =  p.matcher(line);
-        if (m.matches()){
-            return lineCase.RETURN;
-        }
+        if (m.matches()){ return LineCase.RETURN; }
+
 
         // var usage call case
-        p =  Pattern.compile("^[\\s]((final)?[\\s]+(int|double|char|boolean|String)[\\s].*|([a-zA-Z]+|[_]+[a-zA-Z]+)[a-zA-Z_0-9]*)");
+        p =  Pattern.compile("[\\s]*((final)?[\\s]+(int|double|char|boolean|String)[\\s].*|(([a-zA-Z]*|[_])[\\w]+)[\\s]+[=].*)");
         m = p.matcher(line);
 
-        if(m.matches()){ return lineCase.VAR_USAGE;}
+        if(m.matches()){ return LineCase.VAR_USAGE;}
 
+        p =  Pattern.compile("[\\s]*[a-zA-Z][\\w]+[(].*[)][\\s]*[;]");
+        m = p.matcher(line);
 
+        if(m.matches()){ return LineCase.FUNCTION_CALL;}
 
-        return null;//TODO take care of return
+        return LineCase.NO_MATCH;//TODO take care of return
     }
 
     private void checkReturnStatment() throws Exception {
