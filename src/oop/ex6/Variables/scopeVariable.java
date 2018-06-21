75283952f737ee77a66efa74b160b09d0ea3eebb
tomernx <tomer.lankri@mail.huji.ac.il>
2018-06-21 13:52:21 +0300

    vs

--- src/oop/ex6/Variables/scopeVariable.java
+++ src/oop/ex6/Variables/scopeVariable.java
@@ -16,18 +16,19 @@
 
 package oop.ex6.Variables;
 
+import oop.ex6.main.compilerExceptions.InvalidVariableUsageException;
+
 public class scopeVariable {
-	static final String DEFAULT_STRING = "\"a-string\"";
-	static final String DEFAULT_CHAR = "\'a\'";
-	static final String DEFAULT_DOUBLE = "0.0";
-	static final String DEFAULT_BOOLEAN = "true";
-	static final String DEFAULT_INT = "0";
+	private static final int NOT_ASSIGNED = -1;
 	boolean isFinal;
 	String name;
 	String myType;
-	private int isAssigned = -1;
+	private int isAssigned = NOT_ASSIGNED;
 
-	enum defaultVal{
+	/*
+	 * A enum class representing  the different string representations for the types of variables available.
+	 */
+	private enum defaultVal{
 		BOOLEAN("boolean","true"), INT("int","0"), DOUBLE("double","0.0"), STRING("String","\"a-string\""), CHAR("char","\'a\'");
 
 		private final String myType;
@@ -38,46 +39,87 @@ public class scopeVariable {
 		public String getDefaultVal(){return defaultVal;}
 	}
 
-
+	/**
+	 * A constructor for the scope variable.
+	 * @param isFinal a flag representing is the variable final.
+	 * @param name a string representing the name of the variable.
+	 * @param type a string representing the type of the variable.
+	 */
 	public scopeVariable(boolean isFinal, String name, String type) {
 		this.isFinal = isFinal;
 		this.name = name;
 		this.myType =type;
 	}
 
-	public scopeVariable(boolean isFinal, String name, String type, int isAssigned) throws Exception{
+	/**
+	 * A constructor for the scope variable.
+	 * @param isFinal a flag representing is the variable final.
+	 * @param name a  string representing the name of the variable.
+	 * @param type a string representing the type of the variable.
+	 * @param isAssigned a string representing the line type .
+	 * @throws InvalidVariableUsageException
+	 */
+	public scopeVariable(boolean isFinal, String name, String type, int isAssigned) throws InvalidVariableUsageException{
 		this(isFinal,name,type);
 		if (isFinal && isAssigned == -1) {
-			throw new Exception("final with no value");
+			throw new InvalidVariableUsageException("final with no value");
 		}
 		this.isAssigned = isAssigned;
 
 	}
 
+	/**
+	 * A function that returns whether the variable is assigned or not.
+	 * @return a boolean representing if the var is assigned.
+	 */
 	public boolean isAssigned(){
-		return (isAssigned != -1);
+		return (isAssigned != NOT_ASSIGNED);
 	}
 
+	/**
+	 * A function that returns whether the variable is final or not.
+	 * @return a boolean representing if the var is final.
+	 */
 	public boolean isFinal() {
 		return isFinal;
 	}
 
+	/**
+	 * A function that returns the Name of the variable.
+	 * @return
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 *  A function the returns the type of the variable.
+	 * @return A string representation of the variable type.
+	 */
 	public String getMyType() {
 		return myType;
 	}
 
+	/**
+	 * Sets the field isAssigned with a new value.
+	 * @param assigned the value to insert.
+	 */
 	public void setAssigned(int assigned) {
 		isAssigned = assigned;
 	}
 
+	/**
+	 * A method that returns the line which the variable was assigned at.
+	 * @return the line the variable was assigned at.
+	 */
 	public int getVarLineNum(){
 		return isAssigned;
 	}
 
+	/**
+	 * A function that returns a default value of a variable by its type in a string representation.
+	 * @return A relevant default Value.
+	 */
 	public String getDefaultVal(){
 		if(myType.equals(defaultVal.STRING.getType())){
 			return defaultVal.STRING.getDefaultVal();
@@ -95,6 +137,11 @@ public class scopeVariable {
 			return defaultVal.INT.getDefaultVal();
 		}
 	}
+
+	/**
+	 * A function the return whether the variable can be interpreted as a Boolean or not.
+	 * @return true or false.
+	 */
 	public Boolean isBoolean(){
 		return ((myType.equals(defaultVal.BOOLEAN.getType())||myType.equals(defaultVal.INT.getType())||
 				myType.equals(defaultVal.DOUBLE.getType()))&& isAssigned());
--- src/oop/ex6/main/compilerExceptions/InvalidLineException.java
+++ src/oop/ex6/main/compilerExceptions/InvalidLineException.java
@@ -1,6 +1,6 @@
 package oop.ex6.main.compilerExceptions;
 public class InvalidLineException extends Exception {
     private static final long serialVersionUID = 1L;
-    public InvalidLineException() { super(); }
-    public InvalidLineException(String message) { super(message);}
+    public InvalidLineException(){super();}
+    public InvalidLineException(String message){super(message);}
 }
